838ee5990e0570f8d01e3abad552f319d23a439c
galuelx <galulex@gmail.com>
2013-11-11 21:42:26 +0200

    update icons

--- background.js
+++ background.js
@@ -1,20 +1,18 @@
-function isGmailUrl(url) {
+function isVkUrl(url) {
   return url.indexOf("http://vk.com/audio") == 0;
 }
 
-function goToInbox() {
-  console.log('Going to inbox...');
+function removeAudio() {
   chrome.tabs.getAllInWindow(undefined, function(tabs) {
     for (var i = 0, tab; tab = tabs[i]; i++) {
-      if (tab.url && isGmailUrl(tab.url)) {
+      if (tab.url && isVkUrl(tab.url)) {
         chrome.tabs.executeScript(tab.id, {
           code: "var a = document.getElementsByClassName('audio current')[0].getElementsByClassName('audio_remove')[0], evObj = document.createEvent('Events'); evObj.initEvent('click', true, false); a.dispatchEvent(evObj);"
         });
         return;
       }
     }
-    chrome.tabs.create({url: getGmailUrl()});
   });
 }
 
-chrome.browserAction.onClicked.addListener(goToInbox);
+chrome.browserAction.onClicked.addListener(removeAudio);
