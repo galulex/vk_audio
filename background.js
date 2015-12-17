bb93838d2d9bd3dfb8b661cfce89cf82c3f4a64e
galulex <alex@active-bridge.com>
2015-12-17 11:56:34 +0200

    Add controls UI

--- background.js
+++ background.js
@@ -7,18 +7,32 @@ function execute(code) {
   });
 }
 
-chrome.browserAction.onClicked.addListener(function(){
-  execute("document.getElementsByClassName('audio current')[0].getElementsByClassName('audio_remove')[0].click()");
-});
+var player = {
+  play: "document.getElementById('ac_play').click()",
+  forward: "document.getElementById('ac_next').click()",
+  backward: "document.getElementById('ac_prev').click()",
+  add: "document.getElementById('ac_add').click()",
+  remove:  "document.getElementsByClassName('audio current')[0].getElementsByClassName('audio_remove')[0].click()"
+}
 
 var actions = {
-  'Play / Pause': "document.getElementById('ac_play').click()",
-  'Next Song': "document.getElementById('ac_next').click()",
-  'Previous Song': "document.getElementById('ac_prev').click()",
-  'Remove': "document.getElementsByClassName('audio current')[0].getElementsByClassName('audio_remove')[0].click()"
+  'Play / Pause': player.play,
+  'Next Song': player.next,
+  'Previous Song': player.prev,
+  'Remove': player.remove,
+  'Add': player.add
 }
 
 chrome.commands.onCommand.addListener(function(command) {
   if (Object.keys(actions).indexOf(command) < 0) return false;
   execute(actions[command]);
 });
+
+window.addEventListener('DOMContentLoaded', function() {
+  buttons = document.getElementsByTagName('button')
+  for(var i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(){
+      execute(player[this.id]);
+    }, false);
+  }
+})
